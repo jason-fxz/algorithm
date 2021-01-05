1b8a8a1e015e277eae13265347dfd584d4d73910
jason-fxz <jason341132@qq.com>
2021-01-05 18:12:51 +0800

    updata

--- geometry/convex-hull.cpp
+++ geometry/convex-hull.cpp
@@ -22,15 +22,14 @@ struct vec { // Vector & Point
 typedef vec poi;
 il db dot(const vec &a,const vec &b) {return a.x*b.x+a.y*b.y;} // 点积
 il db crs(const vec &a,const vec &b) {return a.x*b.y-a.y*b.x;} // 叉积
-il db dis(const poi &a,const poi &b) {return (b-a).len();}
+il db dis(const poi &a,const poi &b) {return (b-a).len();} // 两点距离
 vector<poi> pts,pois; int n;
-
 poi O;
-bool cmpv(poi a,poi b) {
+bool cmpv(poi a,poi b) { // 极角排序（这个 cmp 需要保证所有点在原点一侧）
 	int k=sign(crs(a-O,b-O));
 	return (k==0)?((a-O).len2()<(b-O).len2()):(k>0);
 }
-int relat(const poi &a,const poi &b,const poi &c) { // 三点关系
+int relat(const poi &a,const poi &b,const poi &c) { // 三点关系，判断是凸还是凹
 	return sign(crs(b-a,c-b));
 }
 void Graham(vector<poi> pt,vector<poi> &res) { // Graham 求凸包 (极角排序+单调栈)
@@ -43,15 +42,39 @@ void Graham(vector<poi> pt,vector<poi> &res) { // Graham 求凸包 (极角排序
 		res.push_back(pt[i]);
 	}
 }
-int main() {
-	scanf("%d",&n);
-	pts.resize(n);
-	for(int i=0;i<n;++i) pts[i].input();
-	Graham(pts,pois);
-	db C=0;
-	for(int i=0;i<(int)pois.size();++i) {
-		if(i!=0) C+=dis(pois[i-1],pois[i]);
-		else C+=dis(pois[0],pois.back());
+db Aera(vec a,vec b,vec c) {return abs(crs(b-a,c-a));} // 三角形面积 |(*2)| 
+db Diam(vector<poi> pt) { // 凸包直径 Diameter “旋转卡壳”
+	int n=pt.size(),j=0; db res=0;
+	if(pt.size()==2) return (pt[0]-pt[1]).len2();
+	pt.push_back(pt[0]);
+	for(int i=0;i<n;++i) {
+		while(Aera(pt[i],pt[i+1],pt[(j+1)%n])>=Aera(pt[i],pt[i+1],pt[j])) j=(j+1)%n;
+		res=max(res,max((pt[j]-pt[i]).len2(),(pt[j]-pt[i+1]).len2()));
 	}
-	printf("%.2lf\n",C);
+	return res;
+}
+db Perim(vector<poi> pt) { // 凸包周长 Perimeter 
+	db res=0; int n=pois.size();
+	for(int i=0;i<n;++i) res+=dis(pois[(i==0)?(n-1):(i-1)],pois[i]);
+	return res;
+}
+db Area(const vector<poi> &pts) { // 任意多边形面积  (有向面积：顺负逆正) 
+	db res=0; int n=pts.size();
+	for(int i=1;i<n-1;++i) res+=crs(pts[i]-pts[0],pts[i+1]-pts[0]);
+	return res/2.0;
+}
+poi Grav(const vector<poi> &pts) { // Center of Gravity  多边形质心 
+	poi g; db sumarea=0; int n=pts.size();
+	for(int i=2;i<n;++i) {
+		db S=crs(pts[i-1]-pts[0],pts[i]-pts[0]);
+		g=g+(pts[0]+pts[i-1]+pts[i])*S;
+		sumarea+=S;
+	}
+	return g/(sumarea*3);
+}
+int main() {
+	// Readin 
+	scanf("%d",&n); pts.resize(n);
+	for(int i=0;i<n;++i) pts[i].input();
+
 }
