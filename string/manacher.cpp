3b9fe06a7ef1258d678117f00083bb64d4e48063
jason-fxz <jasonfanxz@gmail.com>
2021-10-22 21:07:16 +0800

    upd modify manacher.cpp

--- string/manacher.cpp
+++ string/manacher.cpp
@@ -12,11 +12,11 @@ int manacher() {
     int len = strlen(s), n = 0;
     str[n++] = '$'; str[n++] = '#';
     for (int i = 0; i < len; i++) str[n++] = s[i], str[n++] = '#'; str[n] = '\0';
-    int mr = 0, ans = 0, id = 0;
+    int mr = 0, ans = 0, mid = 0;
     for (int i = 1; i < n; i++) {
-        p[i] = i < mr ? min(p[2 * id - i], p[id] + id - i) : 1;
+        p[i] = i < mr ? min(p[2 * mid - i], mr - i) : 1;
         while (str[i - p[i]] == str[i + p[i]]) p[i]++;
-        if (p[i] + i > mr) mr = p[i] + i, id = i;
+        if (p[i] + i > mr) mr = p[i] + i, mid = i;
         ans = max(ans, p[i]);
     }
     return ans - 1;
